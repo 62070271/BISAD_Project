d1004cfb8686da3f689d1d7218edd57aa8b22b95
62070271 <62070271@it.kmitl.ac.th>
2021-04-12 17:44:46 +0700

    edit path
    Fong make we fail because his path file

--- TEST/Datepicka_Fong.S.W/index.php
+++ TEST/Datepicka_Fong.S.W/index.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="jquery.min.js"></script>
-    <script src="../../node_modules/jquery/dist/jquery.js"></script>
-    <script src="../../lib/picker.js"></script>
-    <script src="../../lib/picker.date.js"></script>
-    <script src="../../lib/legacy.js"></script>
-    <link rel="stylesheet" href="../../lib/themes/classic.css">
-    <link rel="stylesheet" href="../../lib/themes/classic.date.css">
-    <link rel="stylesheet" href="../../lib/themes/classic.time.css">
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="lib/picker.js"></script>
+    <script src="lib/picker.date.js"></script>
+    <script src="lib/legacy.js"></script>
+    <link rel="stylesheet" href="lib/themes/classic.css">
+    <link rel="stylesheet" href="lib/themes/classic.date.css">
+    <link rel="stylesheet" href="lib/themes/classic.time.css">
     <style>
 
     </style>
