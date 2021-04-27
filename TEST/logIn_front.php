dd12035e7ba747e2e033a1a80ff69b4b3032fc89
fongzxzxzx <fongzxzxzx@gmail.com>
2021-04-27 12:57:38 +0800

    Update login

--- TEST/logIn_front.php
+++ TEST/logIn_front.php
@@ -53,24 +53,26 @@
             <div class='col-2'></div>
             <div class='col-8'>
                 <div class='row mt-5'>
-                    <div class="card pt-0 pb-1 mb-7" style="width: 50vw; height: 35vw; padding: 12px;">
+                    <div class="card pt-0 pb-1 mb-7" style="width: 100%; height: 35vw; padding: 12px; min-height: 625px;">
                         <div class="row">
                             <div class="card-img-left m-0 p-0 col-4">
-                                <img id='imge' style="width:270px;height: 34.9vw;" src="https://images.pexels.com/photos/6249428/pexels-photo-6249428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="sans" />
+                                <img id='imge' style="width:270px;height: 34.9vw; min-height: 622.5px;" src="https://images.pexels.com/photos/6249428/pexels-photo-6249428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="sans" />
                             </div>
                             <div class="col-8">
 
-                                <div class="card-body" style='vertical-align: middle;'>
+                                <div class="card-body " style='vertical-align: middle;'>
                                     <img src="images/20210413885810631.jpg" alt="" width="125" height="125" class="center img-thumbnail border border-white rounded-circle">
                                     <form action="logIn_back.php" method="POST" onsubmit="" name="logIn">
-                                        <h2>Log In</h2>
+                                        <h2 class='my-2'>Log In</h2>
                                         <input type="text" class="form-control" name="email" placeholder="E-mail">
                                         <br><br>
                                         <input type="password" class="form-control" name='password' placeholder="Password">
                                         <br><br>
-                                        <button type="submit" class="btn btn-success justify-content-md-end" name="submit">Log In!</button>
+                                        <button type="submit" class="btn btn-success" name="submit" style="float: right;">Log In!</button>
                                     </form>
-                                    <h5 class="form-label m-2 mt-4">Create your account. <a href="reg_front.php">Sign Up!</a></h5>
+                                    <br>
+                                    <br>
+                                    <h5 class="form-label m-2 mt-4">Create your account. <a href="reg_front.php"><button type="button" class="btn btn-success">Sign Up!</button></a></h5>
                                 </div>
                             </div>
                         </div>
@@ -85,8 +87,8 @@
             if (($_GET["error"]) == "Wrongemailorpassword!") {
                 echo "<script>alert('Wrong email or password')</script>";
             }
-            if (($_GET["error"]) == "Please login before booking the ticket!") {
-                echo "<script>alert('Please login before booking the ticket!')</script>";
+            if (($_GET["error"]) == "emty_input_login") {
+                echo "<script>alert('Please input  email and password for login')</script>";
             }
         }
         ?>
