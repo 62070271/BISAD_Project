b021e82ccf0bc9e6d7fd62b49e4d9d85497ad015
meior2543 <peper.nouy@hotmail.com>
2021-04-28 22:33:37 +0700

    try

--- TEST/uploadslip_back.php
+++ TEST/uploadslip_back.php
@@ -3,7 +3,13 @@ session_start();
 include('dbserver.php');
     if (isset($_POST['submit'])){
         $upload = $_FILES['pic']['name'];
-        $soderid = $_SESSION['order_id'];
+
+        if ($_POST['order_id']){
+            $soderid = $_POST['order_id'];
+        }
+        else{
+            $soderid = $_SESSION['order_id'];
+        }
 
         if($upload != "")
         {
@@ -25,11 +31,14 @@ include('dbserver.php');
 
             $sql = "INSERT INTO SLIP_OF_PAYMENT (picture, time_stamp, order_id)
                     VALUES ('$newName', '$date', '$soderid')";
+            $up_tk_st = "UPDATE ORDERS SET status='In_progress' WHERE order_id=$soderid";
 
-            $result = mysqli_query($db_con, $sql) or die ("Error in query: $sql " . mysqli_error($db_con));
+            $result = mysqli_query($db_con, $sql, $up_tk_st) or die ("Error in query: $sql " . mysqli_error($db_con));
 
             mysqli_close($db_con);
         }
     }
+
+    
     header('Location: uploadslip_front.php');
 ?>
